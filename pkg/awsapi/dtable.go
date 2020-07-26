5eecfc262552f1aa0c8b952b8fde78b5c49796b9
DmitriKo <dmitrikozhevin@gmail.com>
2020-07-26 19:10:07 +0300

    start to implement package

---
+++ pkg/awsapi/dtable.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+//Provides access to DynamoDB table
+type DTable struct {
+	db       *dynamodb.DynamoDB
+	Name     string
+	Region   string
+	Endpoint string
+	PKey     string
+	Skey     string
+}
+
+//DTableConnect option
+func Endpoint(endpoint string) func(*DTable) error {
+	return func(t *DTable) error {
+		t.Endpoint = endpoint
+		return nil
+	}
+}
+
+//DTableConnect option
+func PKey(key string) func(*DTable) error {
+	return func(t *DTable) error {
+		t.PKey = key
+		return nil
+	}
+}
+
+//Connects to DynomoDB table
+func DTableConnect(name string, options ...func(*DTable) error) (*DTable, error) {
+	t := &DTable{Name: name, PKey: "PK"}
+	for _, option := range options {
+		err := option(t)
+		if err != nil {
+			return t, err
+		}
+	}
+	sess, err := session.NewSession(&aws.Config{
+		Endpoint: aws.String(t.Endpoint),
+		Region:   aws.String("us-west-2"),
+	})
+	if err != nil {
+		return t, err
+	}
+	t.db = dynamodb.New(sess)
+	return t, nil
+}
+
+func (t *DTable) Store(item interface{}) error {
+	av, err := dynamodbattribute.MarshalMap(item)
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(t.Name),
+	}
+	resp, err := t.db.PutItem(input)
+	if err != nil {
+		return err
+	}
+	log.Printf("resp is: %s", resp.String())
+	return nil
+}
+
+type Msg struct {
+	PK  string
+	UMS string
+}
+
+//Puts 3 Msg items in table
+func Put3Items() error {
+	t, _ := DTableConnect("main",
+		Endpoint("http://127.0.0.1:8000"))
+
+	msg := Msg{PK: "msg#bar", UMS: "user1#0"}
+	err := t.Store(msg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	err := Put3Items()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
