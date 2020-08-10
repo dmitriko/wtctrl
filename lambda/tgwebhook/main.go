4f37c34513e1e06d94f37b57eb5e1d894846b250
DmitriKo <dmitrikozhevin@gmail.com>
2020-08-10 20:54:22 +0300

    put msg to sqs

--- lambda/tgwebhook/main.go
+++ lambda/tgwebhook/main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
+func putInSQS(text string) error {
+
+	queue_url := os.Getenv("QUEUE_URL")
+	if queue_url == "" {
+		return errors.New("QUEUE_URL env var is not set")
+	}
+
+	sess := session.Must(session.NewSession())
+	svc := sqs.New(sess)
+	send_input := &sqs.SendMessageInput{
+		MessageGroupId: aws.String("tgwebhook"),
+		MessageBody:    aws.String(text),
+		QueueUrl:       aws.String(queue_url),
+	}
+	_, err := svc.SendMessage(send_input)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func handleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
 	log.Println(req.Body)
-	log.Printf("QueueUrl is %s", os.Getenv("QUEUE_URL"))
+
+	err := putInSQS(req.Body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+		}, err
+	}
+
 	res := events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
 		Headers:    map[string]string{"Content-Type": "text/plan; charset=utf-8"},
