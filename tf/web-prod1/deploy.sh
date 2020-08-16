4ca035fad27d37faeacf6e38b66346902bb832a1
DmitriKo <dmitrikozhevin@gmail.com>
2020-08-16 11:11:44 +0300

    clean wsdefault binary after deploy

--- tf/web-prod1/deploy.sh
+++ tf/web-prod1/deploy.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 ./build_lambda.sh && terraform apply -auto-approve
 rm -rf ../../lambda/wsauth/wsauth
-#rm -rf ../../lambda/tgwebhook/tgwebhook
+rm -rf ../../lambda/wsdefault/wsdefault
 cd $CDIR
