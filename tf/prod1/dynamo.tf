45bb62d6c612bf9561f309ec05d5adf92a90fba8
DmitriKo <dmitrikozhevin@gmail.com>
2020-08-19 20:40:41 +0300

    fix dynamodb tf for SK key, misc

--- tf/prod1/dynamo.tf
+++ tf/prod1/dynamo.tf
@@ -3,9 +3,15 @@ resource "aws_dynamodb_table" "main" {
     name          = var.table_name
     billing_mode  = "PAY_PER_REQUEST"
     hash_key      = "PK"
+    range_key     = "SK"
 
     attribute {
         name = "PK"
+        type = "S"
+    }
+
+    attribute {
+        name = "SK"
         type = "S"
     }
 
