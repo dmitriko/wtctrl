1e113acbb5b48bfc8ced9eeb2e4ba9dc368c32db
DmitriKo <dmitrikozhevin@gmail.com>
2020-08-23 00:24:10 +0300

    fix lambda for new ws conn routine

--- lambda/wsconn/main.go
+++ lambda/wsconn/main.go
@@ -3,18 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/dmitriko/wtctrl/pkg/awsapi"
 )
 
+var table *awsapi.DTable
+
+func init() {
+	table, _ = awsapi.NewDTable(os.Getenv("TABLE_NAME"))
+	err := table.Connect()
+	if err != nil {
+		panic("Could not connect to Dynamo")
+	}
+}
+
 func handleRequest(req events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Printf("%+v", req)
+	err := awsapi.HandleWSConnReq(table, req.RequestContext)
+	if err != nil {
+		fmt.Println("ERROR", err.Error())
+	}
 	resp := events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
 		Body:       "ok",
 	}
-	return resp, nil
+	return resp, err
 }
 
 func main() {
