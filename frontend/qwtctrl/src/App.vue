40d8ecbc505321a4bc9a1c6397dec1e5c317a484
DmitriKo <dmitrikozhevin@gmail.com>
2020-09-03 21:04:58 +0300

    initial quasar project

---
+++ frontend/qwtctrl/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="q-app">
+    <router-view v-if="isLoggedIn" />
+    <Login v-else @loggedIn="isLoggedIn = true" />
+  </div>
+</template>
+
+<script>
+
+import Login from 'pages/Login.vue'
+
+export default {
+    name: 'App',
+    components: {Login},
+    data() {
+        return {
+            isLoggedIn: false
+        }
+    }
+}
+</script>
---
+++ frontend/qwtctrl/src/pages/Login.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Login goes here</h1>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
