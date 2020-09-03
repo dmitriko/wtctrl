610536c43cdbcdb50b1edb3fb03f34e58723572a
DmitriKo <dmitrikozhevin@gmail.com>
2020-09-04 00:22:05 +0300

    css for login page

--- frontend/qwtctrl/src/pages/Login.vue
+++ frontend/qwtctrl/src/pages/Login.vue
@@ -1,9 +1,34 @@
 <template>
-    <h1>Login goes here</h1>
+  <q-page class="bg-secondary window-height window-width row justify-center items-start">
+       <q-card square bordered class="q-ma-lg q-pa-lg shadow-1 window-width">
+          <q-card-section>
+            <q-form class="q-gutter-md">
+              <q-input square filled clearable
+                  label="email or phone"
+                  v-model="loginKey"
+                  class="q-ml-xl q-mr-xl" />
+            </q-form>
+          </q-card-section>
+          <q-card-actions class="q-px-md">
+            <q-btn unelevated color="light-green-7" size="lg" class="q-mx-auto"  label="Login" />
+          </q-card-actions>
+       </q-card>
+ </q-page>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+    name: 'Login',
+    data() {
+        return {
+            loginKey: ""
+        }
+    },
+    methods: {
+        requestOtp() {
+        },
+        sendOtp() {
+        }
+    }
 }
 </script>
