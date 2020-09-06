a39990289db16a9107aa4efea60d1e85b5926be2
DmitriKo <dmitrikozhevin@gmail.com>
2020-09-06 13:36:26 +0300

    change main layout

--- frontend/qwtctrl/src/layouts/MainLayout.vue
+++ frontend/qwtctrl/src/layouts/MainLayout.vue
@@ -1,101 +1,31 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header class="bg-primary text-white">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="isDrawerOpen = !isDrawerOpen"
-        />
+        <q-btn dense flat round icon="menu" @click="isDrawerOpen = !isDrawerOpen" />
 
         <q-toolbar-title>
-            Trust with Web Tech Control
+         Trust with Web Tech Control
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      ref="drawer"
-      v-model="isDrawerOpen"
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+    <q-drawer overlay behavior="desktop" v-model="isDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
     </q-drawer>
 
     <q-page-container>
         <SysMsg />
       <router-view />
     </q-page-container>
+
   </q-layout>
 </template>
-
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
 
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
   created() {
       this.$store.dispatch('ui/closeDrawer')
   },
@@ -111,7 +41,6 @@ export default {
   },
   data () {
     return {
-      essentialLinks: linksData
     }
   }
 }
