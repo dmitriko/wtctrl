564d088ae12a4dce07a3534ad8399bb801690235
DmitriKo <dmitrikozhevin@gmail.com>
2020-09-09 12:10:57 +0300

    start to work on msg editor

---
+++ frontend/qwtctrl/src/components/MsgViewEdit.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <q-card>
+        <q-card-section>{{item.pk}}</q-card-section>
+        <q-card-section>
+            <q-input
+                v-model="text"
+                filled
+                autogrow
+             />
+        </q-card-section>
+        <q-card-section>
+              <q-btn label="Foo" />
+        </q-card-section>
+    </q-card>
+
+</template>
+<script>
+export default {
+    name: 'MsgViewEdit',
+    created() {
+        console.log('in msg edit created')
+    },
+    data() {
+        return {
+            text: this.item.text
+        }
+    },
+    props: {
+        "item": {required: true}
+    }
+}
+</script>
