ed4f4314d6a01b91bd7b9d15bf57b67c024872f4
DmitriKo <dmitrikozhevin@gmail.com>
2020-10-12 14:40:10 +0300

    minor re-arrange

--- tf/msgs-prod1/build_sox.sh
+++ tf/msgs-prod1/build_sox.sh
@@ -1,4 +1,4 @@
-docker build -t audiolayer .
+docker build -t audiolayer -f Dockerfile.sox .
 docker run -it --rm  -w /opt/ -v /tmp:/tmp audiolayer cp -r /opt /tmp/opt
 mv /tmp/opt/* ../../lambda/audio/
 chmod +x ../../lambda/audio/sox
